121ec4cad3f29fe97a7e00a2566c1e0b9993bbe9
Sheridan Roberts <sroberts@centresource.com>
2012-10-08 23:11:00 -0500

    Added wanted Gems, loaded CoffeeScript, LESS and Bootstrap. Introduced Capistrano deployment.

---
+++ kladz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kladz::Application.config.secret_token = '2d9d4f39616567200d921696428e32f9c83325ad0dbc37d105bbeb67368eb15497d4285fb72f7f3c778602afad59cb01ec185826728a43d72667e1f7b3e21091'
